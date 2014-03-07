3cfa1e3f944f31ec5fc2696816af27569d984196
Joshua Moody <joshuajmoody@gmail.com>
2014-03-07 13:07:19 +0100

    tests now compat with XTC

--- features/step_definitions/keychain_steps.rb
+++ features/step_definitions/keychain_steps.rb
@@ -1,26 +1,28 @@
+And(/^I set the username to "([^"]*)" and password to "([^"]*)"$/) do |arg1, arg2|
+  keychain_set_password('com.xamarin.lp-simple-example.service', arg1, arg2)
+end
+
+
 Given(/^that the keychain is clear$/) do
-  keychain_clear_accounts_for_service('briar-ios-example.service')
+  keychain_clear_accounts_for_service('com.xamarin.lp-simple-example.service')
 end
 
 Then(/^the keychain should contain the account password "(.*?)" for "(.*?)"$/) do |password, username|
-  actual = keychain_password('briar-ios-example.service', username)
-  puts "actual => '#{actual}'"
-  unless actual == password
-    res = _keychain_get({:service => 'briar-ios-example.service', :account => username})
-    raise "keychain get => #{res}"
-    #screenshot_and_raise "expected '#{password}' in keychain but found '#{actual}'"
+  actual = keychain_password('com.xamarin.lp-simple-example.service', username)
+  if xamarin_test_cloud?
+    if actual.nil?
+      screenshot_and_raise "expected an entry for '#{username}' in the keychain"
+    end
+  else
+    unless actual == password
+      screenshot_and_raise "expected '#{password}' in keychain but found '#{actual}'"
+    end
   end
 end
 
 Given(/^that the keychain contains the account password "(.*?)" for "(.*?)"$/) do |password, username|
   # app uses the first account/password pair it finds, so clear out
   # any preexisting saved passwords for our service
-  keychain_clear_accounts_for_service('briar-ios-example.service')
-  keychain_set_password('briar-ios-example.service', username, password)
-end
-
-
-
-And(/^I set the username to "([^"]*)" and password to "([^"]*)"$/) do |arg1, arg2|
-  keychain_set_password('briar-ios-example.service', arg1, arg2)
+  keychain_clear_accounts_for_service('com.xamarin.lp-simple-example.service')
+  keychain_set_password('com.xamarin.lp-simple-example.service', username, password)
 end
